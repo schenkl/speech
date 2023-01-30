3dcefeff33cbce837d2ea2d8baeb52327c2df843
Laurence Schenk <laurence.schenk@gmail.com>
2023-01-30 07:36:30 -0500

    new_programs

--- morning_greeting.py
+++ morning_greeting.py
@@ -90,10 +90,20 @@ light_minutes = light % 60
 
 sunrise_hour =int(Sun_rise) / 60
 sunrise_minutes = int(Sun_rise) % 60
+if sunrise_minutes < 10:
+    sm = '0' + str(sunrise_minutes)
+else:
+    sm = str(sunrise_minutes)
 sunset_hour = int(Sun_set) / 60;
 sunset_minutes = int(Sun_set) % 60
+if sunset_minutes < 10:
+    ss = '0' + str(sunset_minutes)
+else:
+    ss = str(sunset_minutes)
+#pdb.set_trace()
 
-command = "/home/pi/sounds/tts-4.sh Sun rise is at " + str(sunrise_hour) + " " + str(sunset_minutes) + " and sun set is at " + str(sunset_hour) + " " + str(sunset_minutes)
+command = "/home/pi/sounds/tts-4.sh Sun rise is at " + str(sunrise_hour) + " " + sm + " and sun set is at " + str(sunset_hour) + " " + ss
+#command = "/home/pi/sounds/tts-4.sh Sun rise is at " + str(sunrise_hour) + " " + str(sunrise_minutes) + " and sun set is at " + str(sunset_hour) + " " + str(sunset_minutes)
 os.system(command)
 
 command = "/home/pi/sounds/tts-4.sh There are " + str(light_hours) + " hours " + str(light_minutes) + " minutes of daylight today "
@@ -109,20 +119,36 @@ if Current_temperature < 110:
 Expected_Rain1 = get_value("Expected_Rain1")
 messages = Expected_Rain1.split(";")
 Expected_Rain1 = messages[6]
+if (Expected_Rain1 > 2):
+    Expected_Rain1 = messages[7]
 Expected_Rain2 = get_value("Expected_Rain2")
 messages = Expected_Rain2.split(";")
 Expected_Rain2 = messages[6]
+if (Expected_Rain2 > 2):
+    Expected_Rain2 = messages[7]
 Expected_Min_T = get_value("Expected_Min_T")
 messages = Expected_Min_T.split(";")
 Expected_Min_T = messages[6]
+if ((Expected_Min_T > 120) or (Expected_Min_T < -50)):
+    Expected_Min_T = messages[7]
 Expected_Max_T = get_value("Expected_Max_T")
 messages = Expected_Max_T.split(";")
 Expected_Max_T = messages[6]
+if ((Expected_Max_T > 120) or (Expected_Max_T < -50)):
+    Expected_Max_T = messages[7]
+#print(Expected_Max_T)
 
+Expected_Rain1 = Expected_Rain1[:4]
 #print("Expected_Rain1 = ",Expected_Rain1)
+Expected_Rain1 = Expected_Rain1.replace(".","")
+Expected_Rain2 = Expected_Rain2[:4]
+Expected_Rain2 = Expected_Rain2.replace(".","")
 #print("Expected_Rain2 = ",Expected_Rain2)
+Expected_Min_T = Expected_Min_T[:4]
 #print("Expected_Min_T = ",Expected_Min_T)
+Expected_Max_T = Expected_Max_T[:4]
 #print("Expected_Max_T = ",Expected_Max_T)
+#pdb.set_trace()
 
 command = "/home/pi/sounds/tts-4.sh the expected high temperature for today is " + str(Expected_Max_T) + " and the expected low temperature is " + str(Expected_Min_T)
 #print(command)
@@ -137,3 +163,12 @@ else:
 os.system(command)
 
     
+mqtt_error = get_value("Mqtt_dev_error")
+messages = mqtt_error.split(";")
+mqtt_error = messages[6]
+print(mqtt_error)
+if mqtt_error == "2":
+	command = ("/home/pi/sounds/tts-4.sh there are no M Q T T devices off line")
+else :
+	command = ("/home/pi/sounds/tts-4.sh there are some  M Q T T devices off line")
+os.system(command)
